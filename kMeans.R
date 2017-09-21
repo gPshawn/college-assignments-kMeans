9ac397ad02694e6d53534a2193cdced101e5c3d2
Guilherme Passos <guilhermepassos@outlook.com>
2017-09-21 11:35:09 -0300

    Update kMeans.R

--- kMeans.R
+++ kMeans.R
@@ -5,8 +5,8 @@
 
 #    This is an alternative implementation of KMeans clustering method in R. Even though there are many 
 # implementations of this algorithm on internet and in most of programming languages (R has its own as well), 
-# I decided to develop my onw using the classical Iris data set in order to get a strong understading of 
-# this algorithm. For a betterunderstanding of the Naive Bayes Classifier, check this following lecture here: 
+# I decided to develop my onw in order to get a strong understading of this algorithm. For a better understanding of 
+# KMeans method, check this following lecture here: 
 # http://stanford.edu/~cpiech/cs221/handouts/kmeans.html
 # p.s: This is a general implementation of Kmeans
 
