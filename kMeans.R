25405262b3ce11f084fc4fd35b3a51ce6856ade3
Guilherme Passos <guilhermepassos@outlook.com>
2017-09-21 11:42:55 -0300

    Update kMeans.R

--- kMeans.R
+++ kMeans.R
@@ -3,7 +3,12 @@
 
 ###############################
 
-
+#    This is an alternative implementation of KMeans clustering method in R. Even though there are many 
+# implementations of this algorithm on internet and in most of programming languages (R has its own as well), 
+# I decided to develop my onw in order to get a strong understading of this algorithm. For a better understanding of 
+# KMeans method, check this following lecture here: 
+# http://stanford.edu/~cpiech/cs221/handouts/kmeans.html
+# p.s: This is a general implementation of Kmeans
 
 ###############################
 
