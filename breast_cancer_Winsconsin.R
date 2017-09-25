78219fba2bd1b5c270bcc48d714cfd3350dd1fa0
Guilherme Passos <guilhermepassos@outlook.com>
2017-09-25 13:55:25 -0300

    Add files via upload

---
+++ breast_cancer_Winsconsin.R
@@ -0,0 +1,51 @@
+# Breast Cancer Winsconsin problem solved by Guilherme Passos - Classification
+# Federal University of Minas Gerais
+
+###############################
+
+# Breast Cancer Winsconsin
+
+
+BreastCancerWinsconsin <- function(BreastCancer, trainRatio){
+  
+  # Loading packgaes
+  library('RSNNS')
+  library('mclust')
+  
+  # Formatting, mixing and spliting the Winsconsin Breast Cancer DataBase
+  X <- data.matrix(BreastCancer)
+  X[is.na(X)] <- 0
+  X <- X[sample(nrow(X),replace=F,size=nrow(X)),]
+  X <- splitForTrainingAndTest( X[,2:10],  X[,11], ratio=trainRatio)
+  
+  # Getting parameters of class benign (Gaussian Mixture Model)
+  benign <- which(X$targetsTrain==1)
+  modelBenign <- densityMclust(X$inputsTrain[benign,])
+  
+  # Getting parameters of class  malignant (Gaussian Mixture Model)
+  malignant <- which(X$targetsTrain==2)
+  modelMalignant <- densityMclust(X$inputsTrain[malignant,])
+  
+  # Posterior probabilities  for each class
+  PosterioriBenign <- dens(modelName=modelBenign$modelName, data = X$inputsTest, parameters = modelBenign$parameters)
+  PosterioriMalignant <- dens(modelName=modelMalignant$modelName, data = X$inputsTest, parameters = modelMalignant$parameters)
+  
+  # checing which class has the higher posterior probability 
+  predictions <- array(length(PosterioriBenign))
+  for (i in 1:length(PosterioriBenign)){
+    if (PosterioriBenign[i]>PosterioriMalignant[i]) predictions[i]<-1
+    else predictions[i]<-2
+  }
+  
+  # 
+  pred <- list(xTrain=X$inputsTrain,yTrain = X$targetsTrain,xTest=X$inputsTest,yTest =X$targetsTest,pred= predictions)
+  
+  return (pred)
+  
+}
+
+
+
+solution <- BreastCancerWinsconsin(data("BreastCancer"),0.3)
+
+
